6a74f75f6956be1471cd50b1dc93009aec3ec3f2
Rudra13 <rudranshlohomi@gmail.com>
2025-04-02 18:50:59 +0530

    changefir

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from models import StudentModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+print("psycopg2 is installed successfully!")
 
 
 
